902724d2a79fabae8c5b7d6728799c44e4f5f9c5
sussy-balls-impostor <eo2dog192@gmail.com>
2022-05-26 21:31:17 +0200

    tracer but it works

--- src/tracer.java
+++ src/tracer.java
@@ -7,11 +7,14 @@ public class tracer extends Rechteck {
                 Math.sqrt(Math.pow(px-mx, 2)+Math.pow(py-my, 2)), //pythagora's theorem bs
                 0.2
         );
-        double angle = Math.asin((py-my)/Math.sqrt(Math.pow(px-mx, 2)+Math.pow(py-my, 2)))*57.296;
+        double angle = Math.asin((py-my)/Math.sqrt(Math.pow(px-mx, 2)+Math.pow(py-my, 2)))*57.296; // i hate trig (math people suck)
         if ((px-mx)>0) setzeDrehwinkel(angle);
         else setzeDrehwinkel(-angle);
 
-        setzeMittelpunkt(((mx-px)/2-0.5), ((my-py)/2)-0.5);
+        setzeMittelpunkt(
+                px+((mx-px)/2-0.5),
+                py+((my-py)/2-0.5)
+        );
         setzeFarbe("Gelb");
         machePartikel(0.3);
         verzoegere(0.2, this::entfernen);
