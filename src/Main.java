e503d9b92112250bad4a9228b5178d62349a5097
Luca <luca.milde@gmx.net>
2022-05-29 21:20:00 +0200

    18 Cowboys...

--- src/Main.java
+++ src/Main.java
@@ -6,6 +6,7 @@ import ea.event.MouseClickListener;
 
 public class Main {
     public static void main(String[] args) {
-        game sUs = new game(1280, 720);
+        //game sUs = new game(1280, 720);
+
     }
 }
