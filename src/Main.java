782b3adfc6ab05b41eb4d587acc63912b8a89567
schlingel <83114842+schlingelTV@users.noreply.github.com>
2022-05-30 19:13:43 +0200

    menu

--- src/Main.java
+++ src/Main.java
@@ -7,11 +7,11 @@ import ea.event.MouseClickListener;
 public class Main {
     public static void main(String[] args) {
         game sUs = new game(1280, 720);
-        for (int x = 0; x < 2; x++) {
-            Thread thread = new Thread(() -> {
-                ohNo fuckfuckfuck = new ohNo();
-            });
-            thread.start();
-        }
+        //for (int x = 0; x < 2; x++) {
+          //  Thread thread = new Thread(() -> {
+           //     ohNo fuckfuckfuck = new ohNo();
+           // });
+            //thread.start();
+       // }
     }
 }
