eb38d887e7bba2d6a4a9029ac3b30e90b9b16eb0
schlingel <83114842+schlingelTV@users.noreply.github.com>
2022-06-04 16:23:21 +0200

    clean

--- README.md
+++ README.md
@@ -1,2 +1,34 @@
 # game-sus
 game sus among sus
+
+public class game extends Scene implements MouseClickListener, FrameUpdateListener {
+public game() {
+p1 = new player();
+this.add(p1);
+p1.setCenter(0,0);
+Game.setDebug(true);
+}
+player p1;
+Polygon bruh;
+@Override
+public void onMouseDown(Vector v, MouseButton mb) {
+if (mb == MouseButton.LEFT) {
+bruh = new Polygon(
+p1.getCenter(),
+p1.getCenter().add(0, 0.05f),
+v,
+v.add(0, 0.05f)
+);
+bruh.setColor(Color.red);
+this.add(bruh);
+}
+
+    }
+
+    @Override
+    public void onFrameUpdate(float v) {
+        try {bruh.remove();}
+        catch (Exception ignored) {}
+    }
+}
+
--- src/game.java
+++ src/game.java
@@ -54,17 +54,10 @@ public class game extends Spiel {
 
 
     void menuScene() {
-        /*
-        Kreis k1 = new Kreis(3);
-        Kreis k2 = new Kreis(2);
-        k1.setzeFarbe("gelb");
-        k2.setzeFarbe("rot");
-        k1.macheStatisch();
-        k2.macheStatisch();
-        if (k1.schneidet(k2)) {
-            System.out.println("alhflidwsufhvnbwlivweuvhdbvlweiuvwdbv");
-        }
-        */
+
+
+
+
     }
 
     private void shoot(double x, double y) {
@@ -95,50 +88,3 @@ public class game extends Spiel {
 
 
 }
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-/*
-public class game extends Scene implements MouseClickListener, FrameUpdateListener {
-    public game() {
-        p1 = new player();
-        this.add(p1);
-        p1.setCenter(0,0);
-        Game.setDebug(true);
-    }
-    player p1;
-    Polygon bruh;
-    @Override
-    public void onMouseDown(Vector v, MouseButton mb) {
-        if (mb == MouseButton.LEFT) {
-            bruh = new Polygon(
-                    p1.getCenter(),
-                    p1.getCenter().add(0, 0.05f),
-                    v,
-                    v.add(0, 0.05f)
-            );
-            bruh.setColor(Color.red);
-            this.add(bruh);
-        }
-
-    }
-
-    @Override
-    public void onFrameUpdate(float v) {
-        try {bruh.remove();}
-        catch (Exception ignored) {}
-    }
-}
-*/
-
