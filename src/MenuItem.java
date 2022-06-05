ce1b8e7484370a9c33dd581cbdccc5c0c09ed791
Luca <luca.milde@gmx.net>
2022-06-05 22:36:50 +0200

    TitleScreen(Graphics)

--- src/MenuItem.java
+++ src/MenuItem.java
@@ -5,10 +5,11 @@ import java.awt.*;
 
 public class MenuItem extends Rectangle {
     private Text lable;
-    public MenuItem(String center, String lableText){
+    public MenuItem(double x , double y, String lableText){
 
         Text lable = new Text(lableText, 1);
         lable.setzeSichtbar(true);
+        lable.setzeMittelpunkt(x, y);
         System.out.println("f");
 
     }
--- src/TitleScreen.java
+++ src/TitleScreen.java
@@ -1,10 +1,19 @@
+import ea.edu.Rechteck;
+
 import java.awt.*;
 
 public class TitleScreen {
 
     TitleScreen(){
 
-        MenuItem m1 = new MenuItem("center","boh");
+        Rechteck button[] = new Rechteck[2];
+        button[0] = new Rechteck(6, 1.5);
+        button[0].setzeMittelpunkt(0, 0);
+        button[1] = new Rechteck(6, 1.5);
+        button[1].setzeMittelpunkt(0, 2);
+        MenuItem m1 = new MenuItem(0, 7,"Boh!");
+        MenuItem b1 = new MenuItem(0, 2,"Play");
+        MenuItem b2 = new MenuItem(0, 0,"Options");
 
     }
 
