a7caab291dcb361b7aa8498e414fd6fd287cf1cf
Luca <luca.milde@gmx.net>
2022-06-06 16:08:04 +0200

    button

--- src/TitleScreen.java
+++ src/TitleScreen.java
@@ -8,19 +8,21 @@ public class TitleScreen {
 
     TitleScreen(){
 
-        Rechteck button[] = new Rechteck[2];
+        /*Rechteck button[] = new Rechteck[2];
         button[0] = new Rechteck(6, 1.5);
         button[0].setzeMittelpunkt(0, 0);
         button[1] = new Rechteck(6, 1.5);
-        button[1].setzeMittelpunkt(0, 2);
-        MenuItem b1 = new MenuItem(0, 2,"Play");
-        MenuItem b2 = new MenuItem(0, 0,"Options");
+        button[1].setzeMittelpunkt(0, 2);*/
+        Figur buttons[] = new Figur[2];
+        buttons[0] = new Figur("button", "rsc/Play.gif");
+        buttons[0].setzeMittelpunkt(0, 2);
+
         Figur backgroundT = new Figur("background", "rsc/b-o-h.gif");
         Figur backgroundGif = new Figur("background", "rsc/hi.gif");
-        backgroundT.setzeEbenenposition(-98);
+        backgroundT.setzeEbenenposition(-1);
         backgroundT.skaliere(0.25);
         backgroundT.setzeMittelpunkt(0,5);
-        backgroundGif.setzeEbenenposition(-99);
+        backgroundGif.setzeEbenenposition(-2);
         backgroundGif.skaliere(2.5);
         backgroundGif.setzeMittelpunkt(0,0);
 
