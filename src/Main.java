70359be4bbbcc79cb645961b2ccf29b768110416
schlingel <83114842+schlingelTV@users.noreply.github.com>
2022-06-08 22:07:20 +0200

    no sound

--- src/Main.java
+++ src/Main.java
@@ -4,7 +4,7 @@ public class Main {
     public static void main(String[] args) {
         new game(1280, 720);
         Sound s1 =new Sound();
-        s1.sound();
+       // s1.sound();
 
     }
 }
