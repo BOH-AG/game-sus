1f27878c171e5118c85bf4a30a9a8cc215e0a1ea
schlingel <83114842+schlingelTV@users.noreply.github.com>
2022-06-08 22:02:50 +0200

    boh tower

--- src/MenuScene.java
+++ src/MenuScene.java
@@ -1,8 +1,28 @@
 import ea.edu.Figur;
 
-public class MenuScene extends Figur {
+public class MenuScene  {
+
+    public  Figur[] menuButton;
     public MenuScene() {
-        super("normal", "rsc/uhoh.gif");
+        menuButton = new Figur[8];
+
+       menuButton[0] = new Figur("button", "rsc/play.gif");
+       menuButton[0].setzeMittelpunkt(-18,10);
+       menuButton[1] = new Figur("button", "rsc/play.gif");
+       menuButton[1].setzeMittelpunkt(-18,7);
+       menuButton[2] = new Figur("button", "rsc/play.gif");
+       menuButton[2].setzeMittelpunkt(-18,4);
+       menuButton[3] = new Figur("button", "rsc/play.gif");
+       menuButton[3].setzeMittelpunkt(-18,1);
+       menuButton[4] = new Figur("button", "rsc/play.gif");
+       menuButton[4].setzeMittelpunkt(-18,-2);
+       menuButton[5] = new Figur("button", "rsc/play.gif");
+       menuButton[5].setzeMittelpunkt(-18,-5);
+       menuButton[6] = new Figur("button", "rsc/play.gif");
+       menuButton[6].setzeMittelpunkt(-18,-8);
+       menuButton[7] = new Figur("button", "rsc/play.gif");
+       menuButton[7].setzeMittelpunkt(-18,-11);
+
 
     }
 }
