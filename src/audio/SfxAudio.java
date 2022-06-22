d44154e9b3937f3536e7e646e2784ee5d32fd82f
sussy-balls-impostor <eo2dog192@gmail.com>
2022-06-22 21:46:21 +0200

    a

--- src/audio/SfxAudio.java
+++ src/audio/SfxAudio.java
@@ -2,16 +2,22 @@ package audio;
 
 import java.io.BufferedInputStream;
 import java.io.FileInputStream;
+
+import ea.FrameUpdateListener;
+import ea.event.EventListeners;
+import ea.event.FrameUpdateListenerContainer;
 import javazoom.jl.player.Player;
 
-public class SfxAudio {
+public class SfxAudio implements FrameUpdateListenerContainer {
     Thread curThread;
     FileInputStream fis;
     BufferedInputStream bis;
     Player audio;
+    int a;
 
     public SfxAudio(String track) {
         AudioMatrix am = new AudioMatrix();
+        a = 0;
         try {
             fis = new FileInputStream(am.get(track, false));
             bis = new BufferedInputStream(fis);
@@ -21,7 +27,23 @@ public class SfxAudio {
             );
             curThread.setDaemon(true);
             curThread.start();
+            delay(
+                    0.1f,
+                    () -> {curThread = null;}
+            );
+
         }
         catch (Exception ignored) {}
+    }
+
+
+    @Override
+    public EventListeners<FrameUpdateListener> getFrameUpdateListeners() {
+        return null;
+    }
+
+    @Override
+    public FrameUpdateListener delay(float timeInSeconds, Runnable runnable) {
+        return FrameUpdateListenerContainer.super.delay(timeInSeconds, runnable);
     }
 }
