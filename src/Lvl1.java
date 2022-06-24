023e0cd494cbfec257f37ffd67cae42e835c7bd7
Luca <luca.milde@gmx.net>
2022-06-24 13:18:38 +0200

    i forgor

--- src/Lvl1.java
+++ src/Lvl1.java
@@ -9,29 +9,38 @@ public class Lvl1{
     public Lvl1(){
 
         //building a Room
-        walls = new Rechteck[11];
-        walls[0] = new Rechteck(0.2,25);//
+        walls = new Rechteck[15];
+        walls[0] = new Rechteck(.2,25);
         walls[0].setzeMittelpunkt(-18.25, 0);
-        walls[1] = new Rechteck(6,0.2);//
+        walls[1] = new Rechteck(6,.2);
         walls[1].setzeMittelpunkt(8.25,-7.75);
-        walls[2] = new Rechteck(30,0.2);//
+        walls[2] = new Rechteck(30,.2);
         walls[2].setzeMittelpunkt(-5,11);
-        walls[3] = new Rechteck(20,0.2);//
+        walls[3] = new Rechteck(20,.2);
         walls[3].setzeMittelpunkt(15.2,10.1);
-        walls[4] = new Rechteck(0.2, 3);//
+        walls[4] = new Rechteck(.2, 3);
         walls[4].setzeMittelpunkt(5.25,4.75);
-        walls[5] = new Rechteck(0.2, 15);//
+        walls[5] = new Rechteck(.2, 15);
         walls[5].setzeMittelpunkt(10.75,-2);
-        walls[6] = new Rechteck(9, 0.2);//
+        walls[6] = new Rechteck(9, .2);
         walls[6].setzeMittelpunkt(9.75,3.1);
-        walls[7] = new Rechteck(20, 0.2);//
+        walls[7] = new Rechteck(20, .2);
         walls[7].setzeMittelpunkt(15.25,6.25);
-        walls[8] = new Rechteck(20, 0.2);//
+        walls[8] = new Rechteck(20, .2);
         walls[8].setzeMittelpunkt(-3,-9.25);
-        walls[9] = new Rechteck(0.2, 3);//
+        walls[9] = new Rechteck(.2, 3);
         walls[9].setzeMittelpunkt(-12.9,-10.75);
-        walls[10] = new Rechteck(0.2, 3);//
+        walls[10] = new Rechteck(.2, 3);
         walls[10].setzeMittelpunkt(-17,-10.75);
+        walls[11] = new Rechteck(.2, 6);
+        walls[11].setzeMittelpunkt(5.25,-2.25);
+        walls[12] = new Rechteck(.2, 6);
+        walls[12].setzeMittelpunkt(7.25,-2.25);
+        walls[13] = new Rechteck(2, .2);
+        walls[13].setzeMittelpunkt(6.25,.75);
+        walls[14] = new Rechteck(2, .2);
+        walls[14].setzeMittelpunkt(6.25,-5.25);
+
 
         //Setting map boundaries
         boundry = new Rechteck[4];
